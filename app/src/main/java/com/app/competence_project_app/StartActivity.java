9814030b85456934cb3772b3d303a7cafc05415a
szymonaszek01 <235879@edu.p.lodz.pl>
2023-03-28 11:03:15 +0200

    task-002 Importing "hivemq" to the project

--- app/src/main/java/com/app/competence_project_app/StartActivity.java
+++ app/src/main/java/com/app/competence_project_app/StartActivity.java
@@ -2,10 +2,17 @@ package com.app.competence_project_app;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+// import com.hivemq.client.mqtt.*;
+
 public class StartActivity extends AppCompatActivity {
+
+//    private String clientId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +22,9 @@ public class StartActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+//        clientId = MqttClient.generateClientId();
+//
+//        onButtonConnectEventListener();
         onButtonBackEventListener();
     }
 
@@ -24,4 +34,36 @@ public class StartActivity extends AppCompatActivity {
             onBackPressed();
         });
     }
+
+//    private void onButtonConnectEventListener() {
+//        Button button = findViewById(R.id.outlinedButtonConnect);
+//        button.setOnClickListener(view -> {
+//            TextInputEditText textInputEditText = findViewById(R.id.edittext_server_uri);
+//            String serverUri = String.valueOf(textInputEditText.getText());
+//
+//            if (!serverUri.isEmpty()) {
+//                connectToBroker(serverUri);
+//            }
+//        });
+//    }
+//
+//    private void connectToBroker(String serverUri) {
+//        try {
+//            MqttAndroidClient client = new MqttAndroidClient(this.getApplicationContext(), serverUri, clientId);
+//            IMqttToken token = client.connect();
+//            token.setActionCallback(new IMqttActionListener() {
+//                @Override
+//                public void onSuccess(IMqttToken asyncActionToken) {
+//                    Log.d(TAG, "Success");
+//                }
+//
+//                @Override
+//                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+//                    Log.d(TAG, "Failure");
+//                }
+//            });
+//        } catch (MqttException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
