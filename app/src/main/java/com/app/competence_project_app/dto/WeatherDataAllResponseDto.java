da6a03d44e0fd6d18e7eb0df5f7d2fd86dcda7bf
szymonaszek01 <235879@edu.p.lodz.pl>
2023-05-07 18:34:44 +0200

    task-006 Adding constructor, getters and setters to WeatherDataAllResponseDto

--- app/src/main/java/com/app/competence_project_app/dto/WeatherDataAllResponseDto.java
+++ app/src/main/java/com/app/competence_project_app/dto/WeatherDataAllResponseDto.java
@@ -16,4 +16,34 @@ public class WeatherDataAllResponseDto {
 
     @SerializedName("data")
     List<Data> dataList;
+
+    public WeatherDataAllResponseDto(RequestDateTime requestDateTime, Sensor sensor, List<Data> dataList) {
+        this.requestDateTime = requestDateTime;
+        this.sensor = sensor;
+        this.dataList = dataList;
+    }
+
+    public RequestDateTime getRequestDateTime() {
+        return requestDateTime;
+    }
+
+    public void setRequestDateTime(RequestDateTime requestDateTime) {
+        this.requestDateTime = requestDateTime;
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public List<Data> getDataList() {
+        return dataList;
+    }
+
+    public void setDataList(List<Data> dataList) {
+        this.dataList = dataList;
+    }
 }
